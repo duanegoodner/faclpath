70a61d4d3e4147b5b5be8db9aa85d33b63a300c5
Duane Goodner <dmgoodner@gmail.com>
2023-02-26 00:02:30 -0700

    move parsing of linux getfacl stdout to a classmethod in ACLData

--- src/data_containers.py
+++ src/data_containers.py
@@ -1,5 +1,7 @@
+import re
 from dataclasses import dataclass
 from file_setting import MaskSetting, PermissionSetting
+from getfacl_output_spec import getfacl_output_items
 
 
 @dataclass
@@ -17,11 +19,27 @@ class ACLData:
     default_group_permissions: PermissionSetting | None
     default_other_permissions: PermissionSetting | None
 
+    @classmethod
+    def from_getfacl_cmd_output(cls, cmd_output: str):
+        kwargs = {}
+        for item in getfacl_output_items:
+            matched_vals = re.findall(
+                item.regex, cmd_output, flags=re.MULTILINE
+            )
+            item.validate_matches(matched_vals)
+            kwargs[item.attribute] = item.to_dict_entry(matched_vals)
+        return cls(**kwargs)
 
-@dataclass
+
 class GetFaclResult:
-    acl_data: ACLData
-    _raw_std_out: str
+
+    def __init__(self, raw_std_out: str, acl_data: ACLData):
+        self._acl_data = acl_data
+        self._raw_std_out = raw_std_out
+
+    @property
+    def acl_data(self) -> ACLData:
+        return self._acl_data
 
     def __str__(self):
-        return self._raw_std_out
+        return self._raw_std_out.strip()
--- src/faclpath.py
+++ src/faclpath.py
@@ -1,17 +1,12 @@
-import re
 import stat
 import subprocess
-from data_containers import ACLData
-from getfacl_output_spec import getfacl_output_items
+from data_containers import ACLData, GetFaclResult
 from pathlib import Path
 
 
 class ACLPath(type(Path())):
-    _access_types = ["user", "group", "other"]
-    _null_access = "---"
 
-    @property
-    def _raw_facl(self) -> str:
+    def _call_getfacl_subprocess(self) -> str:
         command = ["getfacl", str(self)]
         return subprocess.run(
             command,
@@ -19,23 +14,16 @@ class ACLPath(type(Path())):
             stderr=subprocess.PIPE,
         ).stdout.decode("utf-8")
 
-    @property
-    def get_facl(self) -> ACLData:
-        kwargs = {}
-        for item in getfacl_output_items:
-            matched_vals = re.findall(
-                item.regex, self._raw_facl, flags=re.MULTILINE
-            )
-            item.validate_matches(matched_vals)
-            kwargs[item.attribute] = item.to_dict_entry(matched_vals)
+    def getfacl(self) -> GetFaclResult:
+        raw_output = self._call_getfacl_subprocess()
+        acl_data = ACLData.from_getfacl_cmd_output(raw_output)
+        return GetFaclResult(raw_std_out=raw_output, acl_data=acl_data)
 
-        return ACLData(**kwargs)
-
-    @property
-    def filemode(self):
+    def standard_filemode(self) -> str:
         return stat.filemode(self.stat().st_mode)
 
 
 if __name__ == "__main__":
     demo_path = str(Path(__file__).parent.parent / "demo")
     my_acl_dir = ACLPath(demo_path)
+    result = my_acl_dir.getfacl()
